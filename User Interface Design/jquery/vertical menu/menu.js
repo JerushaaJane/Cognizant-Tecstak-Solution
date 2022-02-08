6360a170e21e030c558d1a798473eb8bb0508004
JerushaaJane <67820488+JerushaaJane@users.noreply.github.com>
2022-02-09 00:22:29 +0530

    Add files via upload

---
+++ User Interface Design/jquery/vertical menu/menu.js
@@ -0,0 +1,5 @@
+var element = $('div > ul >li >a')
+$("#each_ex").click(function(){
+  element.css('background-color','red')
+  $('#msg_ex').append('Home<br>Products<br>Services<br>About<br>Contact')
+});
