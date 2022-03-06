315cf5bf07f598fec1bb2554f01b2a30f21c1831
JerushaaJane <67820488+JerushaaJane@users.noreply.github.com>
2022-03-06 08:19:41 +0530

    Create ValidateUtility.java

---
+++ Advanced Java Concepts/Validate Name/ValidateUtility.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateUtility
+{
+    public static void main(String args[])
+    {
+        //fill code here
+        Scanner scanner = new Scanner(System.in);
+        String employee = scanner.nextLine();
+        String product = scanner.nextLine();
+
+        Validate employeeValidation = validateEmployeeName();
+        boolean validEmployee = employeeValidation.validateName(employee);
+
+        if (validEmployee) {
+            System.out.println("Employee name is valid");
+        } else {
+            System.out.println("Employee name is invalid");
+        }
+
+        Validate productValidation = validateProductName();
+        boolean validProduct = productValidation.validateName(product);
+
+        if (validProduct) {
+            System.out.println("Product name is valid");
+        } else {
+            System.out.println("Product name is invalid");
+        }
+    }
+    
+    public static Validate validateEmployeeName() 
+    {
+        //fill code here
+        return name -> {
+            Pattern pattern = Pattern.compile("^[a-zA-Z\\s]{5,20}$");
+            Matcher matcher = pattern.matcher(name);
+
+            return matcher.matches();
+        };
+    }
+    
+    public static Validate validateProductName() 
+    {
+        //fill code here
+        return name -> {
+            Pattern pattern = Pattern.compile("^[a-zA-Z]\\d{5}$");
+            Matcher matcher = pattern.matcher(name);
+
+            return matcher.matches();
+        };
+    }
+}
