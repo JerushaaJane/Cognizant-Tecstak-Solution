ac434cfa577c896cde6e84ce9cb9a547ab3320ac
JerushaaJane <67820488+JerushaaJane@users.noreply.github.com>
2022-03-26 14:32:32 +0530

    Update Vehicle.java

--- Spring Core/EZEE Transport/Vehicle.java
+++ Spring Core/EZEE Transport/Vehicle.java
@@ -4,23 +4,27 @@ import java.util.Map;
 
 public class Vehicle {
 	private String source;
-	private Map<String, Double> destinationMap ;
-	
+	private Map<String, Double> destinationMap;
+
 	public Vehicle(String source) {
 		super();
 		this.source = source;
 	}
+
 	public String getSource() {
 		return source;
 	}
+
 	public void setSource(String source) {
 		this.source = source;
 	}
+
 	public Map<String, Double> getDestinationMap() {
 		return destinationMap;
 	}
+
 	public void setDestinationMap(Map<String, Double> destinationMap) {
 		this.destinationMap = destinationMap;
 	}
-	
+
 }
